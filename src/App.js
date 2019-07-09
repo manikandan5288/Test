94a01f845ac3032f878c052e2355d3d55f202341
Manikandan Velmurugan <manikandan5288@yahoo.com>
2019-07-10 01:57:19 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Test</h1>
+        <div class="content-wrapper">Content</div>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/atoms/SearchBox/Searchbox.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class SearachBox extends React.Component {
+  render() {
+    return <input id="countryName" placeholder="Country Name" type="text" />;
+  }
+}
+export default SearachBox;
